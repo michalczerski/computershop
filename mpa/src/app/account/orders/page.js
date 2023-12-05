dcb836d458f725af0ba211346bfa07f235de804e
micze <czerski.michal@gmail.com>
2023-12-05 16:04:14 +0100

    Order page

--- mpa/src/app/account/orders/page.js
+++ mpa/src/app/account/orders/page.js
@@ -1,6 +1,7 @@
-import './account.scss'
 import {cookies} from "next/headers";
 import Image from "next/image";
+
+
 
 export default async function Page() {
     const user = JSON.parse(cookies().get('user').value);
@@ -9,46 +10,67 @@ export default async function Page() {
             'user-id': user._id
         }
     });
-    const orders = await res.json();
+    let orders = await res.json();
 
-    const formatDate = (date) => {
-        const leadingZeros = n =>  n < 10 ? `0${n}` : n;
-        const obj = new Date(date);
-        const y = obj.getFullYear();
-        const m = leadingZeros(obj.getMonth() + 1);
-        const d = leadingZeros(obj.getDate());
-        return `${d}-${m}-${y}`;
-    }
+    function List({orders}) {
+        const formatDate = (date) => {
+            const leadingZeros = n =>  n < 10 ? `0${n}` : n;
+            const obj = new Date(date);
+            const y = obj.getFullYear();
+            const m = leadingZeros(obj.getMonth() + 1);
+            const d = leadingZeros(obj.getDate());
+            return `${d}-${m}-${y}`;
+        }
+        const total = (items) => {
+            return items.reduce((acc, item) => acc += (item.qty * item.product.priceUS), 0);
+        }
 
-    const total = (items) => {
-        return items.reduce((acc, item) => acc += (item.qty * item.product.priceUS), 0);
+        return (
+            <>
+                <div className="text-2xl">
+                    <span className="font-semibold">Orders </span>
+                    <span className="text-neutral-500">({orders.length})</span>
+                </div>
+                {orders.map(order =>
+                    <div key={order._id} className="mt-7 hover:shadow-2xl border rounded-md p-5">
+                        <div>
+                            <span className="font-medium text-lg">Order </span>
+                            <span className="text-neutral-400 text-sm">[#{order._id}] </span>
+                            <span className="text-neutral-700 font-medium text-sm">{formatDate(order.date)}</span>
+                        </div>
+                        <ul key={order._id} className="divide-y">
+                            {order.items.map(item =>
+                                <li key={item.product._id} className="flex flex-row justify-between items-center
+                                    pb-5 pt-5">
+                                    <Image src={`http://localhost:3030/images/${item.product.url}`}
+                                           width={64} height={55}
+                                           alt={item.product.name} />
+                                    <div className="flex-grow pl-5">{item.product.name}</div>
+                                    <div className="qty">
+                                        {item.qty} {item.qty > 1 ? 'pieces' : 'piece'}
+                                    </div>
+                                    <div className="w-16 text-right">{item.product.priceUS.toFixed(2)}$</div>
+                                </li>
+                            )}
+                        </ul>
+                        <div className="flex justify-end">
+                            <div className="w-36 flex flex-row justify-between border-t pt-3">
+                                <div className="">Total:</div>
+                                <div className="font-semibold text-right">{total(order.items)}$</div>
+                            </div>
+                        </div>
+
+                    </div>
+                )}
+            </>
+        )
     }
 
     return (
         <>
-            <div id="orders-container">
-                {orders.map(order =>
-                    <div key={order._id}>
-                        <div>Order [#{order._id}] {formatDate(order.date)}</div>
-                        <ul className="order shadow box-border p-20" key={order._id}>
-                            {order.items.map(item =>
-                                <li key={item.product._id}>
-                                    <div className="flex v-center">
-                                        <Image src={`http://localhost:3030/images/${item.product.url}`}
-                                               width={64} height={55}
-                                               alt={item.product.name} />
-                                        <div>{item.product.name}</div>
-                                    </div>
-                                    <div className="flex v-center">
-                                        <div className="qty">{item.qty} piece</div>
-                                        <div>{item.product.priceUS.toFixed(2)}$</div>
-                                    </div>
-                                </li>
-                            )}
-                            <li>podsumowanie {total(order.items)}$</li>
-                        </ul>
-                    </div>
-                )}
+            <div className="w-full">
+                {orders.length > 0 && <List orders={orders} /> }
+                {orders.length === 0 && <div>You don't have any orders.</div>}
             </div>
         </>
     )
--- mpa/src/app/account/page.js
+++ mpa/src/app/account/page.js
@@ -1,7 +1,7 @@
 export default async function Account() {
     return (
         <>
-            Dane kurde nomrlanie
+
         </>
     )
 }
