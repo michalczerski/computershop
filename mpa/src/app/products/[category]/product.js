99486fec6f9308845a0e7cef15637b2e5b122988
micze <czerski.michal@gmail.com>
2023-12-06 08:01:29 +0100

    product, title

--- mpa/src/app/products/[category]/product.js
+++ mpa/src/app/products/[category]/product.js
@@ -5,15 +5,16 @@ export default function Product({ attributes, product }) {
   let path = `http://localhost:3030/images/${product.url}`;
   return (
       <div className="group w-1/3 hover:border-gray-300
-            border border-white rounded-md py-3 px-3">
+            border border-white rounded-md py-3 px-3
+            flex flex-col justify-between">
           <Image src={path} width={256} height={220} alt={product.name} />
-          <div className="pb-2">{product.name}</div>
-            <ul>
-              {Object.keys(attributes).map(name =>
-                  <li key={name} className="text-sm">
-                      <span className="capitalize">{name}</span>: {product[name]}
-                  </li>)}
-            </ul>
+          <div className="pb-2 font-semibold text-base text-neutral-700">{product.name}</div>
+          <ul>
+            {Object.keys(attributes).map(name =>
+              <li key={name} className="text-sm">
+                  <span className="capitalize">{name}</span>: {product[name]}
+              </li>)}
+          </ul>
           <div className="flex flex-row justify-between h-6">
               <span className="pt-2">{product.priceUS.toFixed(2)}$</span>
               <AddToBasket product={product} />
