bd9bc5a4464cd6a51d0ec387b33696d7dd0875ec
micze <czerski.michal@gmail.com>
2023-12-07 19:20:59 +0100

    computer set - adding to basket

--- mpa/src/app/sets/addToBasketButton.js
+++ mpa/src/app/sets/addToBasketButton.js
@@ -1,11 +1,27 @@
 'use client'
 
+import {useCookies} from "next-client-cookies";
+import {useContext} from "react";
+import {Context} from "@/app/context";
+import {addToBasket} from "@/components/product";
+
 export default function AddToBasketButton({computer}) {
+    const cookies = useCookies();
+    const context = useContext(Context);
     const total = Object.keys(computer).reduce((acc, part) => acc += computer[part].priceUS, 0);
+
+    const handleAddToBasket = () => {
+        Object.keys(computer).map(name => {
+            addToBasket(context, cookies, computer[name]);
+        });
+    }
+
     return (
         <div className="mt-3 w-full flex justify-between">
             <div className="text-lg font-extralight">{total.toFixed(2)}$</div>
-            <button className="btn-p-g" onClick={() => alert('test')}>Add to basket</button>
+            <button className="btn-p-g" onClick={handleAddToBasket}>
+                Add to basket
+            </button>
         </div>
     )
 }
